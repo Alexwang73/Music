5301dd9a2bca4716aff508f74a54aae1f481e391
BT_1N3_28 <sample@bths.edu>
2025-03-19 10:49:26 -0400

    added text for playGame() in game class

--- src/Game.java
+++ src/Game.java
@@ -5,15 +5,17 @@ public class Game {
     ArrayList spaces;
     boolean win = true;
 
-
     //methods
     public void playGame() {
+        //while the game is ongoing, print the tiles & guess the song
         while(!win) {
             System.out.println(spaces);
-
+            System.out.println("-----------------------------------");
+            System.out.println("GUESS THE SONG:");
+            //add stuff here
+            System.out.println("-----------------------------------");
         }
     }
 
     //helpers (if needed)
-
 }
