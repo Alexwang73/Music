280e1e08cef0a3ee14ac06efd62c6f9c8436f0b3
Alex Wang <alexwang6655@gmail.com>
2025-04-07 01:44:27 -0400

    finished

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
@@ -5,11 +7,15 @@ public class Game {
     private int[][] board;
     private Scanner scanner = new Scanner(System.in);
     private boolean keys;
+    private ArrayList<Song> songs;
+    private int movementPoints;
 
     public Game(int[][] board, String name) {
         this.board = board;
         this.player = new Player(name, board);
         keys = false;
+        songs = Song.load("src\\top_100_most_streamed_songs_on_spotify.csv");
+        movementPoints = 3; // Start with 3 movement points
     }
 
     public void playGame() {
@@ -17,15 +23,77 @@ public class Game {
 
         while (!win) {
             printBoard();
-            System.out.println("Move (WASD): ");
+            System.out.println("Movement Points: " + movementPoints);
+            System.out.println("Move (WASD) or G to guess a song for movement points: ");
             String key = scanner.nextLine().toLowerCase();
 
-            if (key.equals("w")) player.moveUp();
-            else if (key.equals("s")) player.moveDown();
-            else if (key.equals("a")) player.moveLeft();
-            else if (key.equals("d")) player.moveRight();
-            else System.out.println("Invalid input! Please choose a different move");
+            if(key.equals("g")) {
+                int randomSnippet = (int) (Math.random() * 100) + 1;
+                String formatted = String.format("%03d", randomSnippet);
+                File songFile = new File("src/snippets/snippet_" + formatted + ".wav");
 
+                //get the corresponding song from the csv
+                Song randomSong = songs.get(randomSnippet + 1);
+
+                System.out.println("Playing random song snippet #" + randomSnippet);
+                randomSong.playMusic(songFile);
+
+                try {
+                    System.out.println("Press Enter to stop the playback...");
+                    System.in.read();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+
+                System.out.print("Who is this artist?: ");
+                String response = scanner.nextLine();
+
+                boolean correct = randomSong.checkGuess(response);
+
+                if (correct) {
+                    System.out.println("Correct! You get 3 movement points.");
+                    movementPoints += 3;
+                } else {
+                    System.out.println("Incorrect. You get 1 movement point for trying.");
+                    movementPoints += 1;
+                }
+            }
+            else if (key.equals("w") || key.equals("a") || key.equals("s") || key.equals("d")) {
+                // Check if player has movement points
+                if (movementPoints <= 0) {
+                    System.out.println("You don't have any movement points! Guess a song to earn more.");
+                    continue;
+                }
+
+                // Store original position in case we need to revert
+                int originalRow = player.getCurrentRow();
+                int originalCol = player.getCurrentCol();
+
+                // Try to move
+                if (key.equals("w")) {
+                    player.moveUp();
+                }
+                else if (key.equals("s")) {
+                    player.moveDown();
+                }
+                else if (key.equals("a")) {
+                    player.moveLeft();
+                }
+                else if (key.equals("d")) {
+                    player.moveRight();
+                }
+
+                // Decrement movement points only if move was successful
+                if (originalRow != player.getCurrentRow() || originalCol != player.getCurrentCol()) {
+                    movementPoints--;
+                }
+            }
+            else {
+                System.out.println("Invalid input! Please choose a different move");
+                continue;
+            }
+
+            // Check for special tiles
             if (board[player.getCurrentRow()][player.getCurrentCol()] == 5) {
                 printBoard();
                 System.out.println("You picked up a key. Maybe it can unlock something?");
@@ -34,10 +102,13 @@ public class Game {
             if (board[player.getCurrentRow()][player.getCurrentCol()] == 3 && keys == false) {
                 printBoard();
                 System.out.println("I need a key to open this.");
+                // Move player back to previous position
                 if (key.equals("w")) player.moveDown();
                 else if (key.equals("s")) player.moveUp();
                 else if (key.equals("a")) player.moveRight();
                 else if (key.equals("d")) player.moveLeft();
+                // Refund the movement point since the move was invalid
+                movementPoints++;
             }
             if (board[player.getCurrentRow()][player.getCurrentCol()] == 3 && keys == true) {
                 printBoard();
@@ -47,7 +118,7 @@ public class Game {
             // Win condition (reaching position with a 4)
             if (board[player.getCurrentRow()][player.getCurrentCol()] == 4) {
                 printBoard();
-                System.out.println("Congratulations! You won!");
+                System.out.println("Congratulations! You won with " + movementPoints + " movement points remaining!");
                 win = true;
             }
         }
@@ -80,5 +151,9 @@ public class Game {
 
     public int getCurC() {
         return player.getCurrentCol();
+    }
+
+    public int getMovementPoints() {
+        return movementPoints;
     }
 }
--- src/Song.java
+++ src/Song.java
@@ -2,17 +2,20 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Song extends Playlist {
+public class Song {
     private String name;
     private String filePath;
     private String artist;
 
-    //constructor
     public Song(String name, String filePath, String artist) {
-        this.filePath = filePath;
         this.name = name;
+        this.filePath = filePath;
         this.artist = artist;
+
     }
 
     public String getName() {
@@ -31,21 +34,71 @@ public class Song extends Playlist {
     * Author: Max O'Didily
     *
      */
-    public void playMusic() {
-
+    public void playMusic(File filePath) {
         try {
-            File musicPath = new File(filePath);
-            if(musicPath.exists()) {
-                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+            if (filePath.exists()) {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(filePath);
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
             } else {
-                System.out.println("File not found");
+                System.out.println("File not found: " + filePath);
             }
-
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Error playing song: " + name + " - " + e.getMessage());
         }
     }
+
+
+
+    public boolean checkGuess(String guess) {
+        return guess.trim().equalsIgnoreCase(artist);
+    }
+
+    @Override
+    public String toString() {
+        return "Song: " + name + " by " + artist + " [" + filePath + "]";
+    }
+
+    public static ArrayList<Song> load(String filename) {
+        ArrayList<Song> songs = new ArrayList<>();
+
+        try {
+            File myFile = new File(filename);
+            Scanner fileScanner = new Scanner(myFile);
+            fileScanner.nextLine(); // Skip header
+
+            while (fileScanner.hasNextLine()) {
+                String data = fileScanner.nextLine();
+                String[] splitData = data.split(",");
+
+                if (splitData.length > 3) {
+                    String title = splitData[1].replaceAll("\"", "").trim();
+                    String artist = splitData[3].replaceAll("\"", "").trim();
+
+                    Song s = new Song(title, "", artist);
+                    songs.add(s);
+                }
+            }
+
+            fileScanner.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+
+        // Reverse the list
+        ArrayList<Song> reversed = new ArrayList<>();
+        for (int i = songs.size() - 1; i >= 0; i--) {
+            reversed.add(songs.get(i));
+        }
+
+        for (int i = 0; i < reversed.size(); i++) {
+            String filePath = "snippets\\snippet_" + String.format("%03d", i) + ".wav";
+            Song s = reversed.get(i);
+            reversed.set(i, new Song(s.getName(), filePath, s.getArtist()));
+        }
+
+        return reversed;
+    }
+
 }
